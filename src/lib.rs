0e7fdf92ebdf3e07f06373b5ae5078a0aef8c240
Vo Van Nghia <git@vnghia.com>
2024-10-15 05:08:40 +0200

    futures_lite + tokio

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+#![feature(async_closure)]
+
+use futures_lite::{stream, StreamExt};
+
+async fn test_async_closure() -> Vec<i32> {
+    stream::iter(0..3).then(async |i| i).collect().await
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test() {
+        let result = test_async_closure().await;
+        assert_eq!(result, vec![0, 1, 2]);
+    }
+}
